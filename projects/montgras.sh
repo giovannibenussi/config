7e97f87354582bd5baf129e1a0ef3e70f0ab51cd
Giovanni Benussi <giovanni.benussi@usach.cl>
2016-12-14 22:02:13 -0300

    Corregi carga virtualenv montgras

--- projects/montgras.sh
+++ projects/montgras.sh
@@ -4,5 +4,5 @@ PROJECT_PATH=$BRAINS_PATH/$FOLDER_NAME
 alias cmontgras='cd $PROJECT_PATH'
 alias amontgras='atom $PROJECT_PATH'
 alias fmontgras='cmontgras && amontgras'
-alias vmontgras='source $VIRTUALENV_PATH'
+alias vmontgras='source $VIRTUALENV_PATH/montgras/bin/activate'
 alias lmontgras='python manage.py runserver'
