746668b5a887a25b9c0aee259323cc8b45aa2e66
Giovanni Benussi <giovanni.benussi@usach.cl>
2024-01-12 07:27:43 -0300

    Latest changes

--- alias.sh
+++ alias.sh
@@ -3,3 +3,5 @@ alias v='vim'
 alias vv='/Users/giovannibenussi/.local/bin/lvim'
 alias gs='git status'
 alias atom='sh /Applications/Atom.app/Contents/Resources/app/atom.sh'
+alias tat='tmux new -s `basename $PWD`'
+
