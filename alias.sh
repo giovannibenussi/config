72637d5a9a51fc2601e85ef673e88f7568529977
Giovanni <giovanni.benussi@usach.cl>
2024-03-27 10:13:40 -0300

    Latest changes

--- alias.sh
+++ alias.sh
@@ -3,5 +3,19 @@ alias v='vim'
 alias vv='/Users/giovannibenussi/.local/bin/lvim'
 alias gs='git status'
 alias atom='sh /Applications/Atom.app/Contents/Resources/app/atom.sh'
-alias tat='tmux new -s `basename $PWD`'
+#alias tat='tmux new -s `basename $PWD`'
+alias reset-vercel-token='vercel env remove TURSO_API_TOKEN preview && (echo -n $(turso auth token) | vercel env add TURSO_API_TOKEN preview)'
+alias noti3='noti && sleep 1 && noti && sleep 1 && noti'
 
+# From: https://juliu.is/a-simple-tmux/
+function tat {
+  name=$(basename `pwd` | sed -e 's/\.//g')
+
+  if tmux ls 2>&1 | grep "$name"; then
+    tmux attach -t "$name"
+  elif [ -f .envrc ]; then
+    direnv exec / tmux new-session -s "$name"
+  else
+    tmux new-session -s "$name"
+  fi
+}
---
+++ once.sh
@@ -0,0 +1,19 @@
+# From: https://github.com/so-fancy/diff-so-fancy
+
+git config --global core.pager "diff-so-fancy --paginate | less  -R"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
+
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "11"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.func       "146 bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
